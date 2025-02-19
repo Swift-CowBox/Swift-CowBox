62b02368eda3a6d823343266c3741b7fc707116c
Rick van Voorden <rick@northbronson.com>
2025-02-18 17:24:49 -0800

    migrate away from deprecated function

--- Sources/CowBoxMacros/CowBoxMacro.swift
+++ Sources/CowBoxMacros/CowBoxMacro.swift
@@ -131,6 +131,7 @@ extension CowBoxMacro: MemberMacro {
   public static func expansion(
     of node: AttributeSyntax,
     providingMembersOf declaration: some DeclGroupSyntax,
+    conformingTo protocols: [TypeSyntax],
     in context: some MacroExpansionContext
   ) throws -> [DeclSyntax] {
     guard
