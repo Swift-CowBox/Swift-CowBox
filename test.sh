4d73bfad38b7c1f2d02a9302887a0f60a9153e0a
Rick van Voorden <rick@northbronson.com>
2025-02-27 19:28:17 -0800

    update test.sh

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@
 set -euox pipefail
 
 main() {
-  toolchains="Xcode_15.4 Xcode_16 Xcode_16.1 Xcode_16.2 Xcode_16.3_beta"
-  versions="510.0.0 510.0.1 510.0.2 510.0.3 600.0.0 600.0.1"
+  local toolchains="Xcode_15.4 Xcode_16 Xcode_16.1 Xcode_16.2 Xcode_16.3_beta"
+  local versions="510.0.0 510.0.1 510.0.2 510.0.3 600.0.0 600.0.1"
   
   for toolchain in $toolchains; do
     sudo xcode-select --switch /Applications/${toolchain}.app
