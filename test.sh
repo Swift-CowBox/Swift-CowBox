cd2d6778311b5dd9bfbed1ec7c420a03a1a54aee
Rick van Voorden <rick@northbronson.com>
2025-03-03 18:24:38 -0800

    update test.sh

--- test.sh
+++ test.sh
@@ -18,27 +18,6 @@ package() {
   done
 }
 
-#test() {
-#  local configurations="debug release"
-#  
-#  for configuration in $configurations; do
-#    swift build --configuration ${configuration}
-#    swift test --configuration ${configuration}
-#    swift package clean
-#  done
-#}
-
-#package() {
-#  local versions="510.0.0 510.0.1 510.0.2 510.0.3 600.0.0 600.0.1"
-#  
-#  for version in $versions; do
-#    swift package reset
-#    swift package resolve
-#    swift package resolve --version "$version" swift-syntax
-#    test
-#  done
-#}
-
 main() {
   local toolchains="Xcode_15.4 Xcode_16 Xcode_16.1 Xcode_16.2 Xcode_16.3_beta"
   
